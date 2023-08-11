41ad317020df6dacf47329ffb4c3faa019f7e91a
Charles <charles4927@gmail.com>
2023-08-11 13:58:11 -0300

    obs

--- filme/views.py
+++ filme/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Filme
-import pyodbc
+# from .models import Filme
+# import pyodbc
 
 
 # Create your views here.
@@ -15,33 +15,35 @@ def homepage(request):
     #                  "Database=cdtmes;"
     #                  "UID=admin;"
     #                  "PWD=Admin@Condutec;")
+    #
+    # # dados_conexao = ("Driver={MySQL ODBC 8.0 Unicode Driver};"
+    # #                  "Server=177.47.167.82,3306;"
+    # #                  "protocol=tcpip;"
+    # #                  "Database=cdtmes;"
+    # #                  "UID=admin;"
+    # #                  "PWD=Admin@Condutec;")
+    #
+    # # dados_conexao = ('DRIVER={MySQL ODBC 8.0 Unicode Driver};'
+    # #                  'SERVER=177.47.167.82;'
+    # #                  'PORT=3306;'
+    # #                  'DATABASE=cdtmes;'
+    # #                  'UID=admin;'
+    # #                  'PWD=Admin@Condutec;')
+    #
+    # conexao = pyodbc.connect(dados_conexao)
+    #
+    # cursor = conexao.cursor()
+    #
+    # cursor.execute("SELECT Hora FROM luxor_producao")
+    #
+    # ultimo_ciclo = cursor.fetchall()
+    # # valor_anterior = None
+    # # if valores[-1:] != valor_anterior:
+    # # valores_mostrados['text'] = str(valores[-1:][0][0])
+    # cursor.close()
+    # conexao.close()
 
-    dados_conexao = ("Driver={MySQL ODBC 8.0 Unicode Driver};"
-                     "Server=177.47.167.82,3306;"
-                     "protocol=tcpip;"
-                     "Database=cdtmes;"
-                     "UID=admin;"
-                     "PWD=Admin@Condutec;")
-
-    # dados_conexao = ('DRIVER={MySQL ODBC 8.0 Unicode Driver};'
-    #                  'SERVER=177.47.167.82;'
-    #                  'PORT=3306;'
-    #                  'DATABASE=cdtmes;'
-    #                  'UID=admin;'
-    #                  'PWD=Admin@Condutec;')
-
-    conexao = pyodbc.connect(dados_conexao)
-
-    cursor = conexao.cursor()
-
-    cursor.execute("SELECT Hora FROM luxor_producao")
-
-    ultimo_ciclo = cursor.fetchall()
-    # valor_anterior = None
-    # if valores[-1:] != valor_anterior:
-    # valores_mostrados['text'] = str(valores[-1:][0][0])
-    cursor.close()
-    conexao.close()
+    ultimo_ciclo = ["Não definido", "Não definido",]
 
     context['ultimo_ciclo'] = str(ultimo_ciclo[-1:][0][0])
     return render(request, "homepage.html", context)
--- hashflix/settings.py
+++ hashflix/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'hashflix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 import dj_database_url
 
@@ -106,16 +106,16 @@ import dj_database_url
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cdtmes',
-        'USER': 'admin',
-        'PASSWORD': 'Admin@Condutec',
-        'HOST': '177.47.167.82',
-        'PORT': '3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'cdtmes',
+#         'USER': 'admin',
+#         'PASSWORD': 'Admin@Condutec',
+#         'HOST': '177.47.167.82',
+#         'PORT': '3306',
+#     }
+# }
 
 
 
