88b3e57c551e6647d3afbdb807ba891a96cc30f8
Charles <charles4927@gmail.com>
2023-08-16 10:42:26 -0300

    obs

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -101,6 +101,8 @@ WSGI_APPLICATION = 'hashflix.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
